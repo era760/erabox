1485d81ecedd8133ac26fa34b37e0883ea029c81
era760 <a21-6229@outlook.jp>
2026-06-27 12:03:29 +0900

    最低限の機能を追加　電卓機能を追加

---
+++ erabox.h
@@ -0,0 +1,7 @@
+#ifndef ERABOX_H
+#define ERABOX_H
+
+int run_calc(int argc, char *argv[]);
+int run_todo();
+
+#endif // ERABOX_H
---
+++ todo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "erabox.h"
+
+int run_todo() {
+    printf("TODOリストを起動します。\n");
+    // TODOリストの処理をここに実装する
+    return 0;
+}
